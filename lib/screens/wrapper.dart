cf77573e0b035d79df003005ecfdee1611a5ba80
Sabu George <sabu.george@elucidata.io>
2020-05-26 14:59:17 +0530

    [WIP]:added bottom navigation

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:vegitabledelivery/screens/authenticate/authenticate.dart';
 
-class Wrapper extends StatelessWidget {
+class Wrapper extends StatefulWidget {
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  int _selectedIndex = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+    List<Widget> _widgetOptions = <Widget>[
+      Text(
+        'Index 0: Home',
+      ),
+      Authenticate()
+    ];
+
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 20.0,
+        backgroundColor: Hexcolor('#DFE9AC'),
+        elevation: 50.0,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu, color: Hexcolor('#FFA820'),),
+            title: Text('Items', style: TextStyle(fontSize: 12.0, color: Hexcolor('#FFA820')),),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box, color: Hexcolor('#FFA820'),),
+            title: Text('Account', style: TextStyle(fontSize: 12.0, color: Hexcolor('#FFA820')),),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
